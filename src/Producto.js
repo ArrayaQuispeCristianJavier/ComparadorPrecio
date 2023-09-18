5afdd72de9d3b298761823cf6fbabac0acba9131
Arraya Quispe Cristian Javier <Arraya Quispe Cristian Javier>
2023-09-18 17:39:00 -0300

    Segundo commit

--- src/Producto.js
+++ src/Producto.js
@@ -1,1 +1,22 @@
 /*Definir a producto como clase*/
+let NombreProducto=
+{
+ nombreProducto:""
+}
+let PrecioProducto = 
+{
+ precioProducto: 0
+}
+let NombreComercio = 
+{
+    nombreComercio:""
+}
+const AlmacenProducto = [];
+AlmacenProducto.push(NombreProducto);
+AlmacenProducto.push(precioProducto);
+AlmacenProducto.push(nombreComercio);
+
+
+document.getElementById("NombreProducto").innerHTML = AlmacenProducto [0].NombreComercio;
+document.getElementById("PrecioProducto").innerHTML = AlmacenProducto [0].NombreProducto;
+document.getElementById("NombreComercio").innerHTML = AlmacenProducto [0].NombreComercio;
